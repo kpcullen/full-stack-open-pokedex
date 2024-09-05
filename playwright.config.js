f5d3a33250f2b135d9ab079216098fd5c0155f93
Kevin Cullen <157893369+kpcullen@users.noreply.github.com>
2024-09-05 14:56:06 +0200

    playwright

--- playwright.config.js
+++ playwright.config.js
@@ -25,8 +25,8 @@ module.exports = defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:8082',
-    baseURL: 'http://localhost:8080',
+    baseURL: 'http://127.0.0.1:8080',
+    // baseURL: 'http://localhost:8080',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -73,7 +73,7 @@ module.exports = defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run start',
-    url: 'http://localhost:8080',
+    url: 'http://127.0.0.1:8080',
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
   },
