3b8af54fdef4aea2b19daa62cc0e2d44c82312e5
Kevin Cullen <157893369+kpcullen@users.noreply.github.com>
2024-09-05 21:09:24 +0200

    fix health check

--- app.js
+++ app.js
@@ -11,14 +11,8 @@ app.get('/version', (req, res) => {
   res.send('1') // change this string to ensure a new version deployed
 })
 
-// app.get('/health', (req, res) => {
-//   res.send('perfect')
-// })
-
 app.get('/health', (req, res) => {
-  // eslint-disable-next-line no-constant-condition
-  if (true) throw 'error...  '
-  res.send('ok')
+  res.send('perfect')
 })
 
 app.listen(PORT, () => {
