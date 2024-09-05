b15c8437840961627deb54e983d61082310c460f
Kevin Cullen <157893369+kpcullen@users.noreply.github.com>
2024-09-05 16:57:48 +0200

    added dummy endpoint

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
 
 // get the port from env variable
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT
 
 app.use(express.static('dist'))
+
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
 
 app.listen(PORT, () => {
   /* eslint-disable no-console */
